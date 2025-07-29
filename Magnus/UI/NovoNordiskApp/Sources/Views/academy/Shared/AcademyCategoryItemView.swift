5d289572cf4d097fa40a2511c0df47e98af0f074
Pawe Sałek <pawel.r.salek@gmail.com>
2025-07-29 15:44:36 +0200

    [Academy] - dodanie AcademyCategoryItemView

--- Magnus/UI/NovoNordiskApp/Sources/Views/academy/Shared/AcademyCategoryItemView.swift
+++ Magnus/UI/NovoNordiskApp/Sources/Views/academy/Shared/AcademyCategoryItemView.swift
@@ -1,11 +1,29 @@
 import SwiftUI
 
 struct AcademyCategoryItemView: View {
+    let title: String
+
     var body: some View {
-        Text("Academy Category Item")
+        HStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(Color.novoNordiskTextGrey)
+            Spacer()
+            FAIcon(.circle_arrow_right, size: 24, color: Color.novoNordiskLightBlue)
+        }
+        .frame(maxWidth: .infinity, maxHeight: 75)
+        .padding(.horizontal, 20)
+        .background(Color.white)
+        .cornerRadius(12)
+        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 0)
     }
 }
 
 #Preview {
-    AcademyCategoryItemView()
+    VStack {
+        AcademyCategoryItemView(title: "Diabetologia")
+    }
+    .padding(.horizontal, 20)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color(.systemGray6))
 }
