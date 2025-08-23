d734d78b75d13b9f25f7b1076c9f3012678382af
Pawe Sałek <pawel.r.salek@gmail.com>
2025-08-23 18:40:47 +0200

    [News] dodanie obslugi api oraz walidacja newsa w ViewModel

--- Magnus/Features/Sources/ViewModels/News/NewsAddViewModel.swift
+++ Magnus/Features/Sources/ViewModels/News/NewsAddViewModel.swift
@@ -15,31 +15,71 @@ class NewsAddViewModel: ObservableObject {
     @Published public var attachments: [NewsAttachment] = []
     @Published public var tags: [String] = []
 
+    @Published public var groups: [NewsGroup] = []
+
     private let newsService: ApiNewsService
 
     public init(newsService: ApiNewsService = DIContainer.shared.newsService) {
         self.newsService = newsService
+
+        Task {
+            await loadData()
+        }
     }
 
-    public func addNews() async {
-        // await MainActor.run {
-        //     isLoading = true
-        //     hasError = false
-        //     errorMessage = ""
-        // }
+    public func loadData() async {
+        await MainActor.run {
+            isLoading = true
+            hasError = false
+            errorMessage = ""
+        }
 
-        // do {
-        //     let data: GetNewsResponse = try await newsService.getNews()
+        do {
+            let data: GetGroupsResponse = try await newsService.getGroups()
 
-        //     await MainActor.run {
-        //         news = data.news
-        //         isLoading = false
-        //     }
-        // } catch let error {
-        //     isLoading = false
-        //     errorMessage = error.localizedDescription
-        //     hasError = true
-        //     SentryHelper.capture(error: error, action: "NewsListViewModel.loadData")
-        // }
+            await MainActor.run {
+                groups = data.groups
+                isLoading = false
+            }
+        } catch let error {
+            isLoading = false
+            errorMessage = error.localizedDescription
+            hasError = true
+            SentryHelper.capture(error: error, action: "NewsListViewModel.loadData")
+        }
+    }
+
+    public func sendNews() async {
+        await MainActor.run {
+            isLoading = true
+            hasError = false
+            errorMessage = ""
+        }
+
+        do {
+            try await newsService.addNews(title: title, content: content, image: image, selectedGroups: selectedGroups, attachments: attachments, tags: tags)
+
+            await MainActor.run {
+                isLoading = false
+            }
+        } catch let error {
+            isLoading = false
+            errorMessage = error.localizedDescription
+            hasError = true
+            SentryHelper.capture(error: error, action: "NewsAddViewModel.sendNews")
+        }
+    }
+
+    public func canSendNews() -> Bool {
+
+        if (title.isEmpty || content.isEmpty) {
+            return false
+        }
+
+        if image == nil {
+            return false
+        }
+
+        return true
     }
 }
