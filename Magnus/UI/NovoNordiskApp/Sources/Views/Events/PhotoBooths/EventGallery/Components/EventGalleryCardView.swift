337fc8a6de8dafcfd8a504ca5d22734026880263
Pawe Sałek <pawel.r.salek@gmail.com>
2025-08-24 17:21:43 +0200

    [Photo] Okno galerii ostylowane

--- Magnus/UI/NovoNordiskApp/Sources/Views/Events/PhotoBooths/EventGallery/Components/EventGalleryCardView.swift
+++ Magnus/UI/NovoNordiskApp/Sources/Views/Events/PhotoBooths/EventGallery/Components/EventGalleryCardView.swift
@@ -14,34 +14,62 @@ struct EventGalleryCardView: View {
     }
 
     var body: some View {
-        GeometryReader { geometry in
-            // Layout constants
-            let columnsCount: CGFloat = 3
-            let spacing: CGFloat = 3
-            let horizontalPadding: CGFloat = 0
-            let totalSpacing = spacing * (columnsCount - 1)
-            let contentWidth = geometry.size.width - (horizontalPadding * 2)
-            let itemSize = (contentWidth - totalSpacing) / columnsCount
 
-            ScrollView {
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: spacing), count: Int(columnsCount)), spacing: spacing) {
-                    ForEach(gallery) { photo in
-                        EventPhotoItem(photo: photo, size: itemSize) {
-                            // Forward delete action for the item
-                            deleteAction()
+            VStack(alignment: .leading) {
+                galleryTitle
+                GeometryReader { geometry in
+                    // Layout constants
+                    let columnsCount: CGFloat = 3
+                    let spacing: CGFloat = 3
+                    let horizontalPadding: CGFloat = 0
+                    let totalSpacing = spacing * (columnsCount - 1)
+                    let contentWidth = geometry.size.width - (horizontalPadding * 2)
+                    let itemSize = (contentWidth - totalSpacing) / columnsCount
+
+                    ScrollView {
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: spacing), count: Int(columnsCount)), spacing: spacing) {
+                            ForEach(gallery) { photo in
+                                EventPhotoItem(photo: photo, size: itemSize) {
+                                    // Forward delete action for the item
+                                    deleteAction()
+                                }
+                                .onTapGesture { displayAction() }
+                            }
                         }
-                        .onTapGesture { displayAction() }
+                        .padding(.horizontal, horizontalPadding)
+                        .padding(.vertical, 4)
                     }
                 }
-                .padding(.horizontal, horizontalPadding)
-                .padding(.vertical, 4)
+                .padding(.horizontal, 10)
+                .padding(.top, 0)
+                .padding(.bottom, 20)
             }
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(Color.white)
+            .cornerRadius(25)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+    }
+
+    @ViewBuilder
+    private var galleryTitle: some View {
+        HStack {
+            HStack {
+                FAIcon(.location, type: .regular, size: 20, color: Color.novoNordiskBlue)
+                Text(LocalizedStrings.eventPhotoScreenTitle)
+                    .font(.novoNordiskSectionTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.novoNordiskBlue)
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 16)
+            Spacer()
         }
+        .background(Color.novoNordiskLighGreyForPanelBackground)
     }
 }
 
 #Preview("EventGalleryCardView") {
-    let items: [ConferenceEventPhotoBooth] = (1...17).map { i in
+    let items: [ConferenceEventPhotoBooth] = (1...37).map { i in
         ConferenceEventPhotoBooth(id: "\(i)", image: "https://nncv2-dev.serwik.pl/images/1_Gratulujemy_Zwyciezcom_CDC_Poland.jpg")
     }
     VStack {
