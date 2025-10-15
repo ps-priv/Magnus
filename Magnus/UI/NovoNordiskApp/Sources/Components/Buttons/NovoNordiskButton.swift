010d1089447bfb3b3f4c9064ee902e0ece1ea4e5
Pawe Sałek <pawel.r.salek@gmail.com>
2025-10-15 09:46:54 +0200

    [Button] Poprawka stylu tekstu we nnbutton

--- Magnus/UI/NovoNordiskApp/Sources/Components/Buttons/NovoNordiskButton.swift
+++ Magnus/UI/NovoNordiskApp/Sources/Components/Buttons/NovoNordiskButton.swift
@@ -22,7 +22,7 @@ struct NovoNordiskButton: View {
     var body: some View {
         Button(action: action) {
             Text(title)
-                .font(.EveacademyAcademy)
+                .font(.novoNordiskButton)
                 .foregroundColor(style.textColor)
                 .frame(maxWidth: .infinity)
                 .frame(height: 50)
