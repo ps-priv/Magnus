ded4f0b2a6afc755ad6dcc6903c3f01d04d71019
Pawe Sałek <pawel.r.salek@gmail.com>
2025-10-30 14:08:03 +0100

    [App] Build 33

--- Project.swift
+++ Project.swift
@@ -39,7 +39,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleDisplayName": "NovoNordiskApp",
-                    "CFBundleVersion": "32",
+                    "CFBundleVersion": "33",
                     "NSExtension": [
                         "NSExtensionPointIdentifier": "com.apple.usernotifications.service",
                         "NSExtensionPrincipalClass": "$(PRODUCT_MODULE_NAME).NotificationService",
@@ -138,7 +138,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleShortVersionString": "1.0",
-                    "CFBundleVersion": "32",
+                    "CFBundleVersion": "33",
                     "UISupportedInterfaceOrientations": [
                         "UIInterfaceOrientationPortrait"
                     ],
